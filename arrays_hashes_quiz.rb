ff077aeed6ef31046654648daed92c4caafd2656
Zack <Zackcolemanmusic@gmail.com>
2017-09-27 17:29:40 +0100

    Part C, Q5

--- arrays_hashes_quiz.rb
+++ arrays_hashes_quiz.rb
@@ -82,7 +82,7 @@ puts users["Erik"][:favourite_numbers]
 # 4. Return the type of Avril's pet Colin
 puts users["Avril"][:pets]["colin"]
 # 5. Return the smallest of Erik's favorite numbers
-
+puts users["Erik"][:favourite_numbers].min
 # 6. Add the number `7` to Erik's favorite numbers
 
 # 7. Change Erik's hometown to Edinburgh
