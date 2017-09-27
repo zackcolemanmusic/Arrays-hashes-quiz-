122bab459f28384c54e4298b241faa151024b1dc
Zack <Zackcolemanmusic@gmail.com>
2017-09-27 17:06:21 +0100

    my_hash returns, Q4

--- arrays_hashes_quiz.rb
+++ arrays_hashes_quiz.rb
@@ -35,7 +35,7 @@ my_hash[:two]
 # 3. How would you return the number `2`?
 my_hash["two"]
 # 4. How would you add `{3 => "Three"}` to the hash?
-
+my_hash[3] = "Three"
 # 5. How would you add `{:four => 4}` to the hash?
 
 ### C. Given the following data structure:
